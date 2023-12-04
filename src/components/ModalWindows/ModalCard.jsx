400b43fd2a76690a8bd614d3b699d00a64b19942
ArIonMark1 <andreydatsenko777@gmail.com>
2023-12-04 13:35:36 +0100

    new features

--- src/components/ModalWindows/ModalCard.jsx
+++ src/components/ModalWindows/ModalCard.jsx
@@ -1,12 +1,29 @@
-import { createPortal } from "react-dom";
-import css from "./ModalCard.module.css";
-import SvgIcons from "../SvgIcons/SvgIcons";
-import { ModalContent } from "../ModalContent/ModalContent";
+import { createPortal } from 'react-dom';
+import css from './ModalCard.module.css';
+import SvgIcons from '../SvgIcons/SvgIcons';
+import { ModalContent } from '../ModalContent/ModalContent';
+import { useEffect } from 'react';
 
-export const Modal = ({ carData, isOpen, closeFunc, children }) => {
+export const Modal = ({ isOpen, closeFunc }) => {
   if (!isOpen) return null;
+  const handleClose = (evt) => {
+    if (evt.target.className === css.bg_window) {
+      closeFunc();
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyPress = (evt) => {
+      if (evt.key === 'Escape') closeFunc();
+    };
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  });
+
   return createPortal(
-    <div className={css.bg_window}>
+    <div className={css.bg_window} onClick={handleClose}>
       <div className={css.modal}>
         <button className={css.close_btn} onClick={closeFunc}>
           <span>
--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,17 +1,21 @@
-import { useState } from "react";
-import { CatalogFilter } from "../components/CatalogFilter/CatalogFilter";
-import { CatalogList } from "../components/CatalogList/CatalogList";
-import { Modal } from "../components/ModalWindows/ModalCard";
+import { useState } from 'react';
+import { CatalogFilter } from '../components/CatalogFilter/CatalogFilter';
+import { CatalogList } from '../components/CatalogList/CatalogList';
+import { Modal } from '../components/ModalWindows/ModalCard';
 
 const CatalogPage = () => {
   const [open, setOpen] = useState(false);
+  const handleVisibleModal = () => {
+    setOpen(!open);
+  };
   return (
     <>
       <h1>Catalog Page</h1>
-      <button onClick={() => setOpen(true)}>Open Modal</button>
+      {/* <button onClick={() => setOpen(true)}>Open Modal</button> */}
+      <button onClick={handleVisibleModal}>Open Modal</button>
       <CatalogFilter />
       <CatalogList />
-      <Modal isOpen={open} closeFunc={() => setOpen(false)} />
+      <Modal isOpen={open} closeFunc={handleVisibleModal} />
     </>
   );
 };
