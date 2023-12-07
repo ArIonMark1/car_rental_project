eb63089dbfba357697f2f484323b62bd7f94a46b
ArIonMark1 <65442269+ArIonMark1@users.noreply.github.com>
2023-12-07 18:48:59 +0000

    Deploying to gh-pages from @ ArIonMark1/car_rental_project@62c4ae769b1e2ac9a87ab9c646bd03339d25355e 🚀

--- assets/FavoritesPage-3d0ff411.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-d08778d6.js";const t=()=>e.jsx(e.Fragment,{children:e.jsx("h1",{children:"Favorite Page"})});export{t as default};
---
+++ assets/FavoritesPage-df3bf7a7.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-7dcb971d.js";const t=()=>e.jsx(e.Fragment,{children:e.jsx("h1",{children:"Favorite Page"})});export{t as default};
